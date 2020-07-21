2c0d1988d16fa75f938264271f10b9dac40970fe
Nuno Marques <ositaka@gmail.com>
2020-07-21 21:00:51 +0100

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,6 @@
 import React from "react"
 import {Link} from "gatsby"
-import { RiHeart2Line } from "react-icons/ri";
+import { RiHeart2Line } from "react-icons/ri"
 
 const Footer = () => (
   <footer className="site-footer">
